7757508db1c5c2b7b4e3dcd27815ec25ead50379
Rajendra Pandit <rajenpandit@gmail.com>
2024-09-03 21:38:31 +0530

    Update code for ending iteration when min 2 elements found

--- DAA/MinMax.py
+++ DAA/MinMax.py
@@ -1,7 +1,14 @@
 
 def minMaxDivide(arr):
-    subArraySize = int(len(arr)/2)
-    if subArraySize > 0:
+    arrayLen = int(len(arr))
+    subArraySize = int(arrayLen/2)
+    # 1,2,3,4,5
+    # 1,2   3,4,5
+    #       3,4   5
+    #
+    # 1,2,3,4
+    # 1,2   3,4
+    if subArraySize > 1:
         leftArr = arr[:subArraySize]
         rightArr = arr[subArraySize:]
         lMin, lMax = minMaxDivide(leftArr)
@@ -13,7 +20,19 @@ def minMaxDivide(arr):
         if max < rMax:
             max = rMax
         return min, max;
-    return arr[0], arr[0]
+    if arrayLen == 3:
+        #arr[0] = 3, arr[1]=4    arr[2] = 5
+        min, max = minMaxDivide(arr[:2]) #[3,4]
+        if arr[2] < min:
+            return arr[2], max 
+        elif arr[2] > max:
+            return min, arr[2]
+        else:
+            return min, max
+
+    if arr[0] < arr[1]:
+       return arr[0], arr[1]
+    return arr[1], arr[0]
 
 if __name__ == "__main__":
     min,max = minMaxDivide([4,5,6,10,2,6,83,11,7,9])
